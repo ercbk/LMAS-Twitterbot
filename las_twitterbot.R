e7511fbf5b0f03ffda0ce8296856d9f29e9d3842
nllspc <nullspaceax0@gmail.com>
2018-04-13 22:12:17 -0400

    Added NULL and NA checks and default image link; made message template only display columns that are non-NULL/NA

--- las_twitterbot.R
+++ las_twitterbot.R
@@ -48,6 +48,7 @@ obj_json <- fromJSON(content_json)
 # Think this is redundant since we used the stop_for_status function in the commented API section. If not, may need an if-stop check.
 api_message <- obj_json$header$status$message
 
+
 # flatten creates a df; some cols have ".$t" in their names
 pet_df <- flatten(obj_json$petfinder$pets$pet) %>%
       rename_at(vars(ends_with(".$t")), ~str_replace(., "\\.\\$t", "")) %>%
@@ -58,55 +59,78 @@ pet_df <- flatten(obj_json$petfinder$pets$pet) %>%
              size = recode(size, "L" = "Large", "S" = "Small",
                            "M" = "Medium", "XL" = "Extra Large"),
              status = recode(status, "A" = "Adoptable", "H" = "Hold",
-                             "P" = "Pending", "X" = "Adopted/Removed"))
+                             "P" = "Pending", "X" = "Adopted/Removed")
+             )
+
 
 
 # Have to unnest options and breeds separately because they may have different nrows
 
-# Different colnames depending on nrows unnested
-pet_options <- pet_df %>% 
-      select(options.option) %>% 
-      unnest %>% 
-      rename_at(vars(matches("\\$t")), ~str_replace(., "\\$t", "options")) %>%
-      rename_at(vars(matches("options.option")), ~str_replace(., "options.option", "options")) %>% 
-      summarize(options = glue::collapse(options, sep = ", "))
+# Dataframe to add columns to
+bot_df <- pet_df %>% 
+      select(name, animal, age, sex, size, link)
 
-pet_breeds <- pet_df %>% 
-      select(breeds.breed) %>% 
-      unnest %>% 
-      rename_at(vars(matches("\\$t")), ~str_replace(., "\\$t", "breeds")) %>%
-      rename_at(vars(matches("breeds.breed")), ~str_replace(., "breeds.breed", "breeds")) %>% 
-      summarize(breeds = glue::collapse(breeds, sep = ", "))
+# Different colnames depending on nrows unnested
+
+if(!is.na(pet_df$options.option)) {
+      pet_options <- pet_df %>% 
+            select(options.option) %>% 
+            unnest %>% 
+            rename_at(vars(matches("\\$t")), ~str_replace(., "\\$t", "options")) %>%
+            rename_at(vars(matches("options.option")), ~str_replace(., "options.option", "options")) %>% 
+            summarize(misc = glue::collapse(options, sep = ", "))
+      
+      bot_df <- bot_df %>% 
+            bind_cols(pet_options) %>% 
+            select(name, animal, misc, everything())
+}
+
+if(!is.na(pet_df$breeds.breed)) {
+      pet_breeds <- pet_df %>% 
+            select(breeds.breed) %>% 
+            unnest %>% 
+            rename_at(vars(matches("\\$t")), ~str_replace(., "\\$t", "breeds")) %>%
+            rename_at(vars(matches("breeds.breed")), ~str_replace(., "breeds.breed", "breeds")) %>% 
+            summarize(`breed(s)` = glue::collapse(breeds, sep = ", "))
+      
+      bot_df <- bot_df %>% 
+            bind_cols(pet_breeds) %>% 
+            select(name, animal, `breed(s)`, everything())
+}
+
+
+message <- bot_df %>% 
+      select_if(~!is.na(.)) %>% 
+      gather %>%
+      add_row(key = "tags", value = "#adoptdontshop #rescue #adoptme #shelterpets") %>% 
+      mutate(message = glue::glue_data(., "
+                                       {key}: {value}
+                                       ")) %>% 
+      summarize(message = glue::collapse(message, sep = "\n"))
 
 
 # Twitter doesn't display images from image links so we have to jump through some hoops and create a temp jpg file to feed to post_tweet
 
-# Some images are blurry on twitter, although haven't seen a bad one from row 3.
-img_df <- pet_df %>%
-      select(media.photos.photo) %>%
-      unnest %>%
-      slice(3)
-img_url <- img_df$`$t`[[1]]
+# Some images are blurry on twitter, but rows 3 seems to work alright
 
-image_obj <- magick::image_read(img_url)
-tmp <- tempfile(fileext=".jpg")
-magick::image_write(image_obj, path = tmp, format = "jpg")
+if(is.null(pet_df$media.photos.photo[[1]])) {
+      # Default photo if no photo is provided
+      img_url <- "http://www.dogsinpictures.com/images/dog-cat-bunny-bird-love.jpg"
+      image_obj <- magick::image_read(img_url)
+      tmp <- tempfile(fileext=".jpg")
+      magick::image_write(image_obj, path = tmp, format = "jpg")
+}else{
+      img_df <- pet_df %>%
+            select(media.photos.photo) %>%
+            unnest %>%
+            slice(3)
+      img_url <- img_df$`$t`[[1]]
+      
+      image_obj <- magick::image_read(img_url)
+      tmp <- tempfile(fileext=".jpg")
+      magick::image_write(image_obj, path = tmp, format = "jpg")
+}
 
-
-message <- pet_df %>% 
-      select(status, age, size, name, sex, mix, animal, link) %>% 
-      bind_cols(pet_breeds, pet_options) %>% 
-      mutate(message = glue::glue("
-                                  {name} is:
-                                  {breeds}
-                                  {options}      
-                                  {sex}
-                                  {age}
-                                  {size}
-                                  #adoptdontshop #rescue #adoptme #shelterpets
-                                  {link}
-                                  ")) %>% 
-      select(message)
 
 # You'll need to create an app at apps.twitter.com and generate a token first. See get_token.R for details.
 post_tweet(message[[1]], media = tmp)
